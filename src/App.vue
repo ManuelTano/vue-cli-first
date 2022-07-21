87df3de834452e2216c5f9dbb9025ba7a68bf7e7
ManuelTano <manueltano98@gmail.com>
2022-07-21 17:02:54 +0200

    Esercizio completato + bonus

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+
+    <MyTitle />
+
+    <MyHero />
+    
+  </div>
+</template>
+
+<script>
+import MyTitle from './components/Title.vue'
+import MyHero from './components/Hero.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyTitle,
+    MyHero
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <img src="../assets/Matera_Luglio_2019.jpeg" alt="">
+
+</template>
+
+<script>
+export default {
+  name: 'MyHero',
+
+}
+</script>
+
+<style>
+body {
+    margin: 0 auto;
+}
+img {
+    margin-top: 25px;
+    width: 100%;
+    height: 700px;
+}
+</style>
